b84aac8b19839a64b7e616876806c30619a5c368
Easton Archer <136001956+EastonArcher@users.noreply.github.com>
2024-03-14 13:26:24 -0500

    Update SportsPredictions.py

--- SportsPredictions.py
+++ SportsPredictions.py
@@ -1,1 +1,8 @@
-
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
